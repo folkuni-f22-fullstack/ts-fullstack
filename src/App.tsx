b5b2ee26d66caa7ae87d75430440b8b633458faa
David Andersson <david.andersson@zocom.se>
2023-11-13 11:38:28 +0100

    Change event example

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,24 @@ function App() {
 		setCount(count + 1)
 	}
 
+	type ButtonClick = React.MouseEventHandler<HTMLButtonElement>
+	const handlePrint: ButtonClick = event => {
+		console.log('Event: ', event.button);
+	}
+
+	type ChangeHandler = React.ChangeEventHandler<HTMLInputElement>
+	const handleSearchChange: ChangeHandler = event => {
+		// TODO: använd söksträngen för att begränsa vad som visas i listan
+		console.log('Change event, value=' + event.target.value);
+	}
+
 	return (
 		<main>
 			<section>
 				<p> The count is: {count} </p>
 				<button onClick={handleIncrease}> Increase </button>
+				<button onClick={handlePrint}> Print something </button>
+				<input onChange={handleSearchChange} type="text" placeholder="Search" />
 			</section>
 			<BookList data={books} />
 		</main>
